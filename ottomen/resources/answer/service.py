e4f6ddb9a69139db900530ba50c284956f90dd9c
marcromeyn <marcromeyn@gmail.com>
2015-09-05 20:58:42 +0200

    Forgot to change output to label in the service

--- ottomen/resources/answer/service.py
+++ ottomen/resources/answer/service.py
@@ -1,24 +1,24 @@
 from ...core import Service
-from .models import Answer, Output
+from .models import Answer, Label
 
 
 class AnswerService(Service):
     __model__ = Answer
 
 
-class OutputService(Service):
-    __model__ = Output
+class LabelService(Service):
+    __model__ = Label
 
-    def save_or_get_outputs(self, output_names):
+    def save_or_get_labels(self, output_names):
         if output_names is None or len(output_names) == 0:
             return []
-        saved_malwares = self.filter(Output.name.in_(output_names)).all()
+        saved_malwares = self.filter(Label.name.in_(output_names)).all()
         saved_names = [x.name for x in saved_malwares]
         malwares_left = [mw for mw in output_names if mw not in saved_names]
         to_save = []
 
         for mw_name in malwares_left:
-            mw = Output(name=mw_name)
+            mw = Label(name=mw_name)
             to_save.append(mw)
             saved_malwares.append(mw)
         self.save(to_save)
