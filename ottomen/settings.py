b633f32b43bfc2fc8500877cf2946d702c53b05a
marcromeyn <marcromeyn@gmail.com>
2015-09-07 17:53:28 +0200

    Added the right Redis configuration

--- ottomen/settings.py
+++ ottomen/settings.py
@@ -4,6 +4,7 @@ PORT = os.getenv('PORT', 5004)
 DEBUG = os.getenv('DEBUG', True)
 SECRET_KEY = 'secret_key'
 
+# Postgres configuration
 if os.environ.get('TRAVIS'):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://postgres@localhost/ottomen')
 elif os.environ.get('LOCAL'):
@@ -11,13 +12,24 @@ elif os.environ.get('LOCAL'):
 else:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://docker:docker@db/ottomen')
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+# Redis configuration
+if os.environ.get('TRAVIS'):
+    REDIS_CONFIGURATION = {'host': 'localhost', 'port': 6379, 'db': 0}
+elif os.environ.get('LOCAL'):
+    REDIS_CONFIGURATION = {'host': 'dockerhost', 'port': 5003, 'db': 0}
+else:
+    REDIS_CONFIGURATION = {'host': 'redis', 'port': 6379, 'db': 0}
 
-MAIL_DEFAULT_SENDER = 'test@example.com'
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 25
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'username'
-MAIL_PASSWORD = 'password'
+
+
+
+# CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+
+# MAIL_DEFAULT_SENDER = 'test@example.com'
+# MAIL_SERVER = 'smtp.gmail.com'
+# MAIL_PORT = 25
+# MAIL_USE_TLS = True
+# MAIL_USERNAME = 'username'
+# MAIL_PASSWORD = 'password'
 
 JSON_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
--- tests/settings.py
+++ tests/settings.py
@@ -3,6 +3,7 @@ import os
 DEBUG = False
 TESTING = True
 
+# Postgres configuration
 SQLALCHEMY_POOL_SIZE = None
 SQLALCHEMY_POOL_TIMEOUT = None
 SQLALCHEMY_POOL_RECYCLE = None
@@ -13,4 +14,12 @@ elif os.environ.get('LOCAL'):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://docker:docker@dockerhost:5003/ottomen')
 else:
     SQLALCHEMY_DATABASE_URI = 'postgresql://docker:docker@dbtest/ottomen'
+
+# Redis configuration
+if os.environ.get('TRAVIS'):
+    REDIS_CONFIGURATION = {'host': 'localhost', 'port': 6379, 'db': 1}
+elif os.environ.get('LOCAL'):
+    REDIS_CONFIGURATION = {'host': 'dockerhost', 'port': 5003, 'db': 1}
+else:
+    REDIS_CONFIGURATION = {'host': 'redis', 'port': 6379, 'db': 1}
 
