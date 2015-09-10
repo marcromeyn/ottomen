a4be3a0f7c002166554f0cdc55c2bb7502c8a8b1
Jan van de Kerkhof <vdkerkhof.j@gmail.com>
2015-09-10 10:46:32 +0200

    added the largest testmethod in history

--- tests/algorithm/__init__.py
+++ tests/algorithm/__init__.py
@@ -4,6 +4,8 @@ from werkzeug.test import Headers, Client
 
 from ottomen.web.api import create_app
 from ottomen.resources.services import *
+from ottomen.resources.models import Worker
+from ottomen.algorithm.experiment import new_task, new_experiment
 from .. import OttomenAppTestCase, settings
 
 
@@ -13,10 +15,10 @@ class OttomenAlgorithmTestCase(OttomenAppTestCase):
     def setUpClass(cls):
         super(OttomenAlgorithmTestCase, cls).setUpClass()
         cls._create_fixtures()
-        exp = experiments.get(1000)
-        experiments.new_mem(exp)
-        task = tasks.get('1000')
-        tasks.new_mem(task)
+        cls.exp = new_experiment(1337, 0.98)
+        worker = workers.save(Worker(id="gayturk", tw_pos=0.9, class_pos=30, class_neg=30))
+        new_task('1337',1337)
+        workers.new_mem(1337, worker)
         # create experiment and task in redis
 
 
