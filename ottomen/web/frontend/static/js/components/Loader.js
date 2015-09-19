02b38306a06eb88b2a085282fc3257c208e16113
Juan Carlos Roca <juancroca@gmail.com>
2015-09-19 13:26:59 +0200

    loader css

--- ottomen/web/frontend/static/js/components/Loader.js
+++ ottomen/web/frontend/static/js/components/Loader.js
@@ -3,9 +3,8 @@ var React = require('react');
 module.exports = React.createClass({
   render: function(){
     return (
-      <div className={"loader " + (this.props.loaded ? "hidden" : "")}>
+      <div className={"absolute-Center loader " + (this.props.loaded ? "hidden" : "")}>
         <img src="static/assets/loader.svg" />
-        <div> Loading Text </div>
       </div>
     )
   }
